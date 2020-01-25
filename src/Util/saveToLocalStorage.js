1b5597861be070070fd4cee0f60faaeaebe6184a
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-26 00:17:45 +0100

    fixed footer scaling on smaller devices

--- src/Util/saveToLocalStorage.js
+++ src/Util/saveToLocalStorage.js
@@ -1,13 +1,14 @@
 const saveToLocalStorage = (data, type) => {
   const getLocalStorageData = localStorage.getItem(type);
   const localStorageData = JSON.parse(getLocalStorageData);
+
   const elementExist = localStorageData.find(el => {
-    return el.id === data.id
+    return el.id === data.id;
   });
 
   if (!elementExist) {
     localStorageData.push({ ...data });
   }
   localStorage.setItem(type, JSON.stringify(localStorageData));
-}
+};
 export default saveToLocalStorage;
