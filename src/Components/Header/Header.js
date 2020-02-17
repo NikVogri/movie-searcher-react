4f91f0e48d8f2472d73dd01803587179b7b3cce3
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-02-17 21:52:45 +0100

    added video as hero

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
 import classes from "./Header.module.css";
-import HeaderImage from "../../img/header.jpg";
+// import HeaderImage from "../../img/header.jpg";
 const Header = () => {
   return (
     <>
       <div className={classes.Header}>
         <h1>Find your favourite films & TV shows!</h1>
-        <img src={HeaderImage} alt="Header" className={classes.Image} />
+        {/* <img src={HeaderImage} alt="Header" className={classes.Image} /> */}
+        <iframe
+          title="header video"
+          className={classes.Image}
+          src="https://www.youtube.com/embed/zAGVQLHvwOY?autoplay=1&mute=1"
+          frameborder="0"
+          autoplay="1"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+
         <div className={classes.ScrollDown}></div>
       </div>
     </>
